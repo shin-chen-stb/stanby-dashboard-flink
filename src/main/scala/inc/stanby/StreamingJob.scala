901ca5e3d6c6102a654a74695787a1ceda8a67b0
Shin Chen <shin.chen@stanby.inc>
2021-12-03 11:08:47 +0900

    Save scala version elasticsearchSink

--- src/main/scala/inc/stanby/StreamingJob.scala
+++ src/main/scala/inc/stanby/StreamingJob.scala
@@ -18,44 +18,38 @@
 
 package inc.stanby
 
+import inc.stanby.operators.AmazonElasticsearchSink;
+import inc.stanby.schema.StanbyEvent;
+import inc.stanby.utils.StanbyEventSchema;
 import org.apache.flink.streaming.api.scala._
+import inc.stanby.schema.StanbyEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Properties;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
 
-/**
- * Skeleton for a Flink Streaming Job.
- *
- * For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 object StreamingJob {
-  def main(args: Array[String]) {
-    // set up the streaming execution environment
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val serviceName = "es";
+  val region = "ap-northeast-1";
+  val inputStreamName = "dmt-dataplatform-analytics-stream";
+  val domainEndpoint = "https://search-chen-stanby-analytics-dev-dqo5rvvb3udyugdkkrrzjr5gda.ap-northeast-1.es.amazonaws.com";
+  val logger = LoggerFactory.getLogger("Stanby_Dashboard_Flink");
 
-    /*
-     * Here, you can start creating your execution plan for Flink.
-     *
-     * Start with getting some data from the environment, like
-     *  env.readTextFile(textPath);
-     *
-     * then, transform the resulting DataStream[String] using operations
-     * like
-     *   .filter()
-     *   .flatMap()
-     *   .join()
-     *   .group()
-     *
-     * and many more.
-     * Have a look at the programming guide:
-     *
-     * https://flink.apache.org/docs/latest/apis/streaming/index.html
-     *
-     */
+  def createStanbyEventSourceFromStaticConfig(env: StreamExecutionEnvironment): DataStream[StanbyEvent] = {  
+        val inputProperties = new Properties();
+        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, this.region);
+        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "TRIM_HORIZON");
+        return env.addSource(new FlinkKinesisConsumer<>("dmt-dataplatform-analytics-stream", new StanbyEventSchema(), inputProperties));
+  }
+
+  def main(args: Array[String]) {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val input = createStanbyEventSourceFromStaticConfig(env);
+		input.addSink(AmazonElasticsearchSink.buildElasticsearchSink(domainEndpoint, this.region, "stanby_event2", "_doc", classOf[StanbyEvent]));
+
+    
 
     // execute program
     env.execute("Flink Streaming Scala API Skeleton")
