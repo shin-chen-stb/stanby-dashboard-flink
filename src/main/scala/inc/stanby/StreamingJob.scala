f974efc20130d192802c1b5abe0bd298474844b2
Shin Chen <shin.chen@stanby.inc>
2021-12-03 14:35:52 +0900

    Add stanby_event sink

--- src/main/scala/inc/stanby/StreamingJob.scala
+++ src/main/scala/inc/stanby/StreamingJob.scala
@@ -18,38 +18,56 @@
 
 package inc.stanby
 
-import inc.stanby.operators.AmazonElasticsearchSink;
-import inc.stanby.schema.StanbyEvent;
-import inc.stanby.utils.StanbyEventSchema;
+import inc.stanby.operators.AmazonElasticsearchSink
+import inc.stanby.schema.StanbyEvent
+import inc.stanby.utils.StanbyEventSchema
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
-import inc.stanby.schema.StanbyEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.Properties;
-import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer
+import org.apache.http.HttpRequestInterceptor
+import org.apache.flink.streaming.connectors.kinesis.config.{AWSConfigConstants, ConsumerConfigConstants}
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
+import java.util.Properties;
+import com.amazonaws.services.kinesisanalytics.runtime.KinesisAnalyticsRuntime
+import inc.stanby.schema.StanbyEvent
+import inc.stanby.utils.StanbyEventSchema
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer
+import java.io.IOException
+import java.util
 object StreamingJob {
   val serviceName = "es";
   val region = "ap-northeast-1";
-  val inputStreamName = "dmt-dataplatform-analytics-stream";
   val domainEndpoint = "https://search-chen-stanby-analytics-dev-dqo5rvvb3udyugdkkrrzjr5gda.ap-northeast-1.es.amazonaws.com";
   val logger = LoggerFactory.getLogger("Stanby_Dashboard_Flink");
 
-  def createStanbyEventSourceFromStaticConfig(env: StreamExecutionEnvironment): DataStream[StanbyEvent] = {  
-        val inputProperties = new Properties();
-        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, this.region);
-        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "TRIM_HORIZON");
-        return env.addSource(new FlinkKinesisConsumer<>("dmt-dataplatform-analytics-stream", new StanbyEventSchema(), inputProperties));
+  private def createStanbyEventSourceFromStaticConfig(env: StreamExecutionEnvironment, inputStreamName: String) = {
+    val inputProperties = new Properties
+    inputProperties.setProperty(AWSConfigConstants.AWS_REGION, region)
+    inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "TRIM_HORIZON")
+    env.addSource(new FlinkKinesisConsumer[StanbyEvent](inputStreamName, new StanbyEventSchema, inputProperties))
   }
 
-  def main(args: Array[String]) {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val input = createStanbyEventSourceFromStaticConfig(env);
-		input.addSink(AmazonElasticsearchSink.buildElasticsearchSink(domainEndpoint, this.region, "stanby_event2", "_doc", classOf[StanbyEvent]));
+  private def createSourceFromStaticConfig(env: StreamExecutionEnvironment, inputStreamName: String) = {
+    val inputProperties = new Properties
+    inputProperties.setProperty(AWSConfigConstants.AWS_REGION, region)
+    inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "TRIM_HORIZON")
+    env.addSource(new FlinkKinesisConsumer[String](inputStreamName, new SimpleStringSchema, inputProperties))
+  }
 
-    
+  @throws[IOException]
+  private def createSourceFromApplicationProperties(env: StreamExecutionEnvironment) = {
+    val applicationProperties = KinesisAnalyticsRuntime.getApplicationProperties
+    env.addSource(new FlinkKinesisConsumer[StanbyEvent]("dmt-dataplatform-analytics-stream", new StanbyEventSchema, applicationProperties.get("ConsumerConfigProperties")))
+  }
+
+  @throws[Exception]
+  def main(args: Array[String]): Unit = { // set up the streaming execution environment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val input = createStanbyEventSourceFromStaticConfig(env, "dmt-dataplatform-analytics-stream")
+    input.addSink(AmazonElasticsearchSink.buildElasticsearchSink(domainEndpoint, region, "stanby_event", "_doc"))
 
     // execute program
     env.execute("Flink Streaming Scala API Skeleton")
